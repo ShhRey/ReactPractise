73ce1b535e055bc3395fb3f7e97e281917fb8bf4
Shrey Trivedi <shreytrivedi577@gmail.com>
2024-07-31 13:59:00 -0400

    Handling Inputs and Errors via Yup

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,18 +1,46 @@
 import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 
 export default function Form() {
-    const { register, handleSubmit } = useForm();
+    // Schema for Validation of Data can also be written via Regex
+    const schema = yup.object().shape({
+        fullName: yup.string().required('Full Name is required'),
+        email: yup.string().email('Must be a valid email').required('Email required'),
+        password: yup.string().min(6, 'at least 6 characters').required('Password required'),
+        confirmPassword: yup.string().oneOf([yup.ref('password'), null], 'Passwords must match').required('Confirm Password is required')
+    });
 
+    // Calling required function from react-hook-form
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        // Specifying the Format of our Form
+        resolver: yupResolver(schema)
+    });
+
+    // Function to Display the user submitted data
     const onSubmit = (data) => {
         console.log(data);
     }
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <input type="text" placeholder="Enter Full Name..." {...register("fullName")} />
-            <input type="text" placeholder="Enter your Email..." {...register("email")} />
-            <input type="password" placeholder="Enter Password..." {...register("pass")} />
-            <input type="password" placeholder="Confirm Password..." {...register("confPass")} />
+        <form style={{marginTop: "20px"}} onSubmit={handleSubmit(onSubmit)}>
+            <h2>React Forms Implementation</h2>
+            <div>
+                <input type="text" placeholder="Enter Full Name..." {...register('fullName')} />
+                {errors.fullName && <p>{errors.fullName.message}</p>}
+            </div>
+            <div>
+                <input type="text" placeholder="Enter your Email..." {...register('email')} />
+                {errors.email && <p>{errors.email.message}</p>}
+            </div>
+            <div>
+                <input type="password" placeholder="Enter Password..." {...register('password')} />
+                {errors.password && <p>{errors.password.message}</p>}
+            </div>
+            <div>
+                <input type="password" placeholder="Confirm Password..." {...register('confirmPassword')} />
+                {errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
+            </div>
             <input type="submit" />
         </form>
     )
