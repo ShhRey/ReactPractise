77e8f3a946ecb9e9fb6a0cd438aeb8d8e82301c8
Shrey Trivedi <shreytrivedi577@gmail.com>
2024-07-31 14:15:29 -0400

    Redundant Files Removed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
     return (
-        <div>
+        <div style={{display: "flex", justifyContent: "space-around"}}>
             <Link to="/">HOME</Link>
             <Link to="/about">ABOUT</Link>
             <Link to="/contact">CONTACT</Link>
